3cda86460ac6a22f72d896965a598e75444d38a2
Cleisson Andrade <cleisson@outlook.com.br>
2021-06-03 14:55:25 -0300

    Atualização no arquivo app.js

--- express-mongodb/rastro/app.js
+++ express-mongodb/rastro/app.js
@@ -1,8 +1,19 @@
 const express = require('express');
-console.log(`express: ${typeof(express)} | constructor: ${express.constructor.name}`);
+// console.log(`express: ${typeof(express)} | constructor: ${express.constructor.name}`);
 
 const app = express();
-console.log(`app: ${typeof(app)} | constructor: ${app.constructor.name}`);
+// console.log(`app: ${typeof(app)} | constructor: ${app.constructor.name}`);
 
-app.listen(3000, () =>console.log('Servidor Online, porta 3000'));
+app.listen(3000, /*porta*/ ()=>console.log('Servidor Online, porta 3000'));
 
+// Criando ROta
+// Rota '/'
+
+// get() define a rota e a função que deve ser executada...
+app.get(
+    '/', // é a URL da rota
+    (request, response) => {
+        console.log('Rota principal chamada...');
+        response.send('Servidor esta ONLINE, aqui vai mandar JSON');
+    }
+);
